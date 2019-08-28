24d105136464941be1aaec5a293269798325e6f5
pagpires <hongxinyupires@gmail.com>
2019-08-27 23:12:24 -0400

    ADD CUDA mode for overfitting test

--- cs224n_2019winter/a4/run.sh
+++ cs224n_2019winter/a4/run.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "train" ]; then
 	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./en_es_data/train.es --train-tgt=./en_es_data/train.en --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --cuda
 elif [ "$1" = "test" ]; then
-        CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./en_es_data/test.es ./en_es_data/test.en ./outputs/test_outputs.txt --cuda
+    CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./en_es_data/test.es ./en_es_data/test.en ./outputs/test_outputs.txt --cuda
 elif [ "$1" = "train_local" ]; then
 	python run.py train --train-src=./en_es_data/train.es --train-tgt=./en_es_data/train.en --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json
 elif [ "$1" = "test_local" ]; then
@@ -19,6 +19,13 @@ elif [ "$1" = "dev_test" ]; then
     python run.py decode model_benchmark.bin ./en_es_data/train_small.es ./en_es_data/train_small.en ./outputs/dev_outputs.txt
 	echo "Performance of trained model"
     python run.py decode model.bin ./en_es_data/train_small.es ./en_es_data/train_small.en ./outputs/dev_outputs.txt
+elif [ "$1" = "dev_train_cuda" ]; then
+	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./en_es_data/train_small.es --train-tgt=./en_es_data/train_small.en --dev-src=./en_es_data/train_small.es --dev-tgt=./en_es_data/train_small.en --vocab=vocab_small.json --cuda
+elif [ "$1" = "dev_test_cuda" ]; then
+	echo "Performance of untrained model from CUDA"
+    CUDA_VISIBLE_DEVICES=0 python run.py decode model_benchmark.bin ./en_es_data/train_small.es ./en_es_data/train_small.en ./outputs/dev_outputs.txt --cuda
+	echo "Performance of trained model from CUDA"
+    CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./en_es_data/train_small.es ./en_es_data/train_small.en ./outputs/dev_outputs.txt --cuda
 else
 	echo "Invalid Option Selected"
 fi
